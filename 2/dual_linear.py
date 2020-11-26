95e94bf5361e8493fe6a40bfce1f7986b2bf9319
Dmitry Alexeev <dalexeev.work@gmail.com>
2020-11-26 18:56:31 +0300

    add file for dual linear program

---
+++ 2/dual_linear.py
@@ -0,0 +1,7 @@
+
+#https://cbom.atozmath.com/CBOM/Simplex.aspx?q=pd&q1=4%602%60MAX%60Z%60x1%2cx2%2cx3%2cx4%601%2c-2%2c2%2c-1%601%2c1%2c0%2c1%3b2%2c0%2c1%2c-1%60%3d%2c%3d%607%2c13%60%60D%60false%60true%60false%60true%60false%60false%60true&do=1#PrevPart
+#https://math.semestr.ru/simplex/msimplex.php
+#https://math.semestr.ru/simplex/lec_dvoistven.php
+
+def dual():
+    print('dual')
