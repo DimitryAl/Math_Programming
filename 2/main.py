bd6887ec22a3bcde527cca5d390dcbaa88ca8ec2
DimitryAl <dalexeev.work@gmail.com>
2020-11-30 12:22:29 +0300

    small changes

--- 2/main.py
+++ 2/main.py
@@ -5,7 +5,7 @@
 # xi >= 0
 
 from simplex_method import simplex
-
+from dual_linear import dual
 print('Исходные данные:')
 print('z = x1 -2x2 + 2x3 - x4 -> extr')
 print('x1 + x2 + x4 = 7\n2x1 + x3 - x4 = 13\nxi >= 0')
@@ -17,8 +17,13 @@ print('2)Двойственная задача\n')
 n = int(input())
 
 if n == 1:
-    print('Решение задачи симплекс методом\n')
+    
     simplex()
-
-
-print('done')
+if n == 2:
+   
+    dual()
+if n == 12:
+    simplex()
+    dual()
+    
+print('\ndone')
