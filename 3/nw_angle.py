9bdfa9df9e8ec592397fbcba261eb872b9b9ced9
Dmitry Alexeev <dalexeev.work@gmail.com>
2020-12-04 16:30:33 +0300

    add potential check ver 1

--- 3/nw_angle.py
+++ 3/nw_angle.py
@@ -17,8 +17,8 @@ def check(i, j, stocks, needs, new_tar):
                         needs[k] = needs[k] - stocks[i]
                         stocks[i] = 0
                         new_tar[i][k] = c
-        if new_tar[i][k] == None:
-            new_tar[i][k] = 'Seen'
+        #if new_tar[i][k] == None:
+        #    new_tar[i][k] = 'Seen'
         if (i, k) != (i, j):
             cnt += 1
     for k in range(len(new_tar[k])):
@@ -35,8 +35,8 @@ def check(i, j, stocks, needs, new_tar):
                         needs[j] = needs[j] - stocks[k]
                         stocks[k] = 0
                         new_tar[k][j] = c 
-        if new_tar[k][j] == None:
-            new_tar[k][j] = 'Seen'
+        #if new_tar[k][j] == None:
+        #    new_tar[k][j] = 'Seen'
         if (k, j) != (i, j):
             cnt += 1
     return cnt
@@ -44,7 +44,7 @@ def check(i, j, stocks, needs, new_tar):
 def nwangle(tariffs, stocks, needs, n):
     
     cnt = 0                 # счетчик пройденных клеток
-    min_tariff = 1          
+    #min_tariff = 1          
     step = 0
     new_tar = copy.deepcopy(tariffs)
     for i in range(n):
@@ -53,39 +53,38 @@ def nwangle(tariffs, stocks, needs, n):
     new_stocks = stocks.copy()
     new_needs = needs.copy()
 
-    # составляем начальный план перевозок
-    # заполняем северо-западный угол
-    if new_stocks[0] - new_needs[0] >= 0:
-        c = new_needs[0]
-        new_stocks[0] = new_stocks[0] - new_needs[0]
-        new_needs[0] = 0
-    else:
-        c = new_stocks[0]
-        new_needs[0] = new_needs[0] - new_stocks[0]
-        new_stocks[0] = 0
-    new_tar[0][0] = c
-    cnt += check(0, 0, new_stocks, new_needs, new_tar)
-    cnt += 1
-    step += 1
-    # заполняем все остальное
     while True:
-        while cnt != n*n:
-            for i in range(n):
-                for j in range(n):
-                    if tariffs[i][j] == min_tariff:
-                        if new_tar[i][j] == None:
-                            if new_stocks[i] - new_needs[j] >= 0:
-                                c = new_needs[j]
-                                new_stocks[i] = new_stocks[i] - new_needs[j]
-                                new_needs[j] = 0
-                            else:
-                                c = new_stocks[i]
-                                new_needs[j] = new_needs[j] - new_stocks[i]
-                                new_stocks[i] = 0
-                            new_tar[i][j] = c 
-                            cnt += check(i, j, new_stocks, new_needs, new_tar)
-            min_tariff += 1
+        # заполнение 
+        for i in range(n):
+            for j in range(n):
+                if i == j:
+                #if new_tar[i][j] == None:
+                    if new_stocks[i] - new_needs[j] >= 0:
+                        c = new_needs[j]
+                        new_stocks[i] = new_stocks[i] - new_needs[j]
+                        new_needs[j] = 0
+                    else:
+                        c = new_stocks[i]
+                        new_needs[j] = new_needs[j] - new_stocks[i]
+                        new_stocks[i] = 0
+                    new_tar[i][j] = c 
+                    cnt += check(i, j, new_stocks, new_needs, new_tar)
+                    cnt += 1
+        # проверка на оптимальность методом потенциалов
+        u = [None for i in range(n)]
+        v = [None for i in range(n)]
+        u[0] = 0            # задаем начальное значение одному из потенциалов
+        for i in range(n):
+             for j in range(n):
+                if new_tar[i][j] != None:
+                    if u[i] != None:
+                        v[j] = tariffs[i][j] - u[i]
+                    else:
+                        u[i] = tariffs[i][j] - v[j]
 
 
+        if (i, j) == (n-1, n-1):
+            break
 
+    print('done')
 #nwangle([],[],[],4)
