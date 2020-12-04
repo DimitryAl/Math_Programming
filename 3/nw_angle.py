da63c1fc2b9d0c09b8530ee333dfe5ec1daf7bd1
Dmitry Alexeev <dalexeev.work@gmail.com>
2020-12-04 19:38:33 +0300

    finish north-west angle method

--- 3/nw_angle.py
+++ 3/nw_angle.py
@@ -4,7 +4,7 @@ import copy
 def check(i, j, stocks, needs, new_tar):
     cnt = 0
     for k in range(len(new_tar[i])):
-        if new_tar[i][k] == None:
+        if new_tar[i][k] == 0:
             if needs[k] != 0:
                 if stocks[i] != 0:
                     if stocks[i] >= needs[k]:
@@ -20,7 +20,7 @@ def check(i, j, stocks, needs, new_tar):
         if (i, k) != (i, j):
             cnt += 1
     for k in range(len(new_tar[k])):
-        if new_tar[k][j] == None:
+        if new_tar[k][j] == 0:
             if needs[j] != 0:
                 if stocks[k] != 0:
                     if stocks[k] >= needs[j]:
@@ -41,7 +41,7 @@ def nwangle(tariffs, stocks, needs, n):
     new_tar = copy.deepcopy(tariffs)
     for i in range(n):
         for j in range(n):
-            new_tar[i][j] = None
+            new_tar[i][j] = 0
     new_stocks = stocks.copy()
     new_needs = needs.copy()
 
@@ -67,24 +67,27 @@ def nwangle(tariffs, stocks, needs, n):
         u = [None for i in range(n)]
         v = [None for i in range(n)]
         u[0] = 0            # задаем начальное значение одному из потенциалов
-        for i in range(n):
-             for j in range(n):
-                if new_tar[i][j] != None:
-                    if u[i] != None:
-                        v[j] = tariffs[i][j] - u[i]
-                    else:
-                        u[i] = tariffs[i][j] - v[j]
+        while (None in u) or (None in v):
+            for i in range(n):
+                for j in range(n):
+                    if new_tar[i][j] != 0:
+                        if u[i] != None:
+                            v[j] = tariffs[i][j] - u[i]
+                        elif v[j] != None:
+                            u[i] = tariffs[i][j] - v[j]
         # оценка незадействованных маршрутов
         min_delta = 0
+        # мб  не искать минимальную, а брать первую
         for i in range(n):
             for j in range(n):
-                if new_tar[i][j] == None:
+                if new_tar[i][j] == 0:
                     delta = tariffs[i][j] - (u[i] + v[j])
                     if min_delta == 0 and delta < 0:
                         min_delta = delta
                         delta_i = i
                         delta_j = j
                     if delta < min_delta :
+                        min_delta = delta
                         delta_i = i
                         delta_j = j
         if min_delta == 0:
@@ -94,10 +97,10 @@ def nwangle(tariffs, stocks, needs, n):
         coordinates[0] = (delta_i, delta_j)
         flag = False
         for i in range(n):
-            if i != delta_i and new_tar[i][delta_j] != None:
+            if i != delta_i and new_tar[i][delta_j] != 0:
                 for j in range(n):
-                    if j != delta_j and new_tar[delta_i][j] != None:
-                        if new_tar[i][j] != None:
+                    if j != delta_j and new_tar[delta_i][j] != 0:
+                        if new_tar[i][j] != 0:
                             coordinates[1] = (i, delta_j)
                             coordinates[2] = (i, j)
                             coordinates[3] = (delta_i, j)
@@ -107,17 +110,19 @@ def nwangle(tariffs, stocks, needs, n):
             if flag:
                 break
         # находим наимегьшую перевозку
-        if coordinates[1] < coordinates[3]:
-            min_value = coordinates[1]
+        if new_tar[coordinates[1][0]][coordinates[1][1]] < new_tar[coordinates[3][0]][coordinates[3][1]]:
+            min_value = new_tar[coordinates[1][0]][coordinates[1][1]]
         else:
-            min_value = coordinates[3]
+            min_value = new_tar[coordinates[3][0]][coordinates[3][1]]
         for i in range(4):
             if i % 2 == 0:
                 new_tar[coordinates[i][0]][coordinates[i][1]] = new_tar[coordinates[i][0]][coordinates[i][1]] + min_value
             if i % 2 == 1:
                 new_tar[coordinates[i][0]][coordinates[i][1]] = new_tar[coordinates[i][0]][coordinates[i][1]] - min_value
 
+    for i in new_tar:
+        print(i)
+    
 
 
     print('done')
-#nwangle([],[],[],4)
