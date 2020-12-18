fdcfe14979f91e5b8caab54472307ea9b3276dc6
Dmitry Alexeev <dalexeev.work@gmail.com>
2020-12-18 22:52:18 +0300

    small changes

--- 2/main.py
+++ 2/main.py
@@ -8,6 +8,7 @@
 
 from simplex_method import simplex
 from dual_linear import dual
+
 print('Исходные данные:')
 print('z = x1 -2x2 + 2x3 - x4 -> extr')
 print('x1 + x2 + x4 = 7\n2x1 + x3 - x4 = 13\nxi >= 0')
@@ -36,10 +37,6 @@ while True:
         break
     if n == 2:
         dual(cons, z, b)
-        break
-    if n == 12:
-        simplex(cons, z, b)
-        #dual()
         break
     print('Wrong number, try again')
 print('\ndone')
